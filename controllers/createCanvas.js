ab8150ddb9c570e2d92041c187b7e033431e3483
Ella Katajisto <ella@frameright.io>
2022-07-22 12:58:31 +0200

    creating a canvas with output image works

--- controllers/createCanvas.js
+++ controllers/createCanvas.js
@@ -1,4 +1,6 @@
-import { createCanvas } from "canvas";
+import { createCanvas, loadImage } from "canvas";
+import fs from "fs";
+// import {  } from "./functionality"
 
 const width = 1200;
 const height = 600;
@@ -6,6 +8,12 @@ const height = 600;
 export function create() {
   const canvas = createCanvas(width, height);
   const context = canvas.getContext("2d");
-  context.fillStyle = "#fff";
+  context.fillStyle = "yellow";
   context.fillRect(0, 0, width, height);
+
+  loadImage("downloadImage.jpeg").then((image) => {
+    context.drawImage(image, 425, 225);
+    const buffer = canvas.toBuffer("image/png");
+    fs.writeFileSync("./image.png", buffer);
+  });
 }
